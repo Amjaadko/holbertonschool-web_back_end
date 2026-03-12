df607372f5c02daaa8ed089c48611d2197c25445
Amjad Al Omani <iamjaadk@gmail.com>
2026-03-12 02:47:27 +0000

    task 1

---
+++ ES6_basic/1-block-scoped.js
@@ -0,0 +1,11 @@
+export default function taskBlock(trueOrFalse) {
+  const task = false;
+  const task2 = true;
+
+  if (trueOrFalse) {
+    const taskBlock = true;
+    const task2Block = false;
+  }
+
+  return [task, task2];
+}
